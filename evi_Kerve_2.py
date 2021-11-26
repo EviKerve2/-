bdb6de52869b56335b826187c5e0814fc96b84a5
opilane <opilane@HP-CZC6297B82>
2021-11-26 10:50:25 +0200

    lkjg

--- evi_Kerve_2.py
+++ evi_Kerve_2.py
@@ -1,41 +1,50 @@
 #1
-def arithmetic(a, b, oper):
-    if oper=="*":
-        return a*b
-    elif oper=="/":
+def arithmetic(a, b, r):
+    if r=="+":
+        return a + b
+    elif r=="-":
+        return a - b
+    elif r=="*":
+        return a * b
+    elif r=="/":
         return a/b
-    elif oper=="+":
-        return a+b
-    elif oper=="-":
-        return a-b
     else:
-        print("Неизвестная операция")
-        return 0
+        return "Неизвестная операция"
 
-##2
+#2
 def is_year_leap(year):
-    if (year%4==0) and (year%100!=0) or (year%400==0):
+
+    if year % 400==0:
         return True
+
+    if year % 4==0 and year % 100!=0:
+        return True
+
     return False
 
 #3
-from math import*
+import math
+
 def square(a):
-    P=4*a
-    S=a**2
+
+    p = 4*a
+    s = a**2
     diag=a*math.sqrt(2)
-    return P, S, diag
+
+    return p, s, diag
+
 
 #4
 def season(month):
     if month in (12, 1, 2):
-        return "Зима"
+        return "зима"
     elif month in (3, 4, 5):
-        return "Весна"
+        return "весна"
     elif month in (6, 7, 8):
-        return "Лето"
+        return "лето"
     elif month in (9, 10, 11):
-        return "Осень"
+        return "осень"
+
 
 #5
 def bank(a, years):
@@ -43,16 +52,51 @@ def bank(a, years):
         a*=1.1
     return a
 
+
 #6
 def is_prime(number):
-    if number==1:
+
+    if number == 1:
         return False
-    for possible_divisor in range(2, x):
-        if (number%possible_divisor==0):
+
+    for divisor in range(2, number):
+        if number % divisor==0:
             return False
     return True
 
+
+#7
+def is_year_leap(year):
+
+    if year % 400 == 0:
+        return True
+
+    if year % 4 == 0 and year % 100!=0:
+        return True
+
+    return False
+
+
+def date(day, month, year):
+    day_in_month = {1: 31,
+                    2: 29 if is_year_leap(year) else 28,
+                    3: 31,
+                    4: 30,
+                    5: 31,
+                    6: 30,
+                    7: 31,
+                    8: 31,
+                    9: 30,
+                    10: 31,
+                    11: 30,
+                    12: 31}
+
+    if 1 <= month <= 12 and 1 <= day <= day_in_month[month]:
+        return True
+    return False
+
 #8
+
 def xor_uncipher(string: str, key:str)->str:
     """Kodeeritud text dekodeeritakse
     """
